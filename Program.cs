9f08f43cfe209412a9e57e409b25d51a9087b87e
Sergio Luiz <sergioj99523@gmail.com>
2022-08-26 22:31:42 -0300

    resolução exercicio de contagagem regressiva

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("preparar ...");
+Console.ReadKey();
+Console.WriteLine("apontar ...");
+Console.ReadKey();
+Console.WriteLine("FOGO !");
+Thread.Sleep(1000);
+Console.Write("Pressione qualquer tecla para continuar");
+Console.ReadKey();
